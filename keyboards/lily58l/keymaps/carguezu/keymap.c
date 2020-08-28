7b3237081b577b1e40f28dc051dc4711cd2a8f94
CarlosGz <carlosgz@me.com>
2020-08-29 01:43:16 +0200

    update modifiers

--- keyboards/lily58l/keymaps/carguezu/keymap.c
+++ keyboards/lily58l/keymaps/carguezu/keymap.c
@@ -16,11 +16,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |  ~`  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  |\  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Tab  |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
- * |------+------+------+------+------+------|   [   |    |S Cmd/]|------+------+------+------+------+------|
- * |LShift|   Z  |   X  |   C  |   V  |S Cmd/B|-------|   |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
+ * |------+------+------+------+------+------|   [   |    | OptSpc|------+------+------+------+------+------|
+ * |LShift|   Z  |   X  |   C  |   V  |S Cmd/B|------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |Cntrl | Opt  |Cmmnd | / Space /       \Enter \  | Cmd  | Alt  | ESC  |
- *                   |      |      |      |/ RAISE /         \RAISE \ |      |      |LOWER |
+ *                   |      | Opt  |Cmmnd | / Space /       \Enter \  | Cmd  | Alt  |      |
+ *                   |      |      | Esc  |/ LOWER /         \LOWER \ | Esc  |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 
@@ -28,8 +28,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
   KC_GRAVE, KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLASH, \
   KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,  SCMD_T(KC_B), KC_LBRC,  SCMD_T(KC_RBRC),  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
-                             KC_LCTRL,KC_LALT, KC_LGUI,LT(_RAISE, KC_SPC),  LT(_RAISE, KC_ENT),  KC_RGUI, KC_RALT, LT(_LOWER, KC_ESC) \
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,  SCMD_T(KC_B), KC_LBRC,  LALT(KC_SPC),  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
+                             _______,KC_LALT, MT(MOD_LGUI, KC_ESC),LT(_LOWER, KC_SPC),  LT(_LOWER, KC_ENT),  MT(MOD_RGUI, KC_ESC), KC_RALT, _______ \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
