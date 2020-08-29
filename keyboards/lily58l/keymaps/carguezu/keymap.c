cdb3492eef49ed9eb82dbefcb61e133a5e5cd727
CarlosGz <carlosgz@me.com>
2020-08-29 02:54:37 +0200

    update lower layer

--- keyboards/lily58l/keymaps/carguezu/keymap.c
+++ keyboards/lily58l/keymaps/carguezu/keymap.c
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Tab  |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------|   [   |    | OptSpc|------+------+------+------+------+------|
- * |LShift|   Z  |   X  |   C  |   V  |S Cmd/B|------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
+ * |LShift|   Z  |   X  |   C  |   V  |S Cmd/B|------|    |-------|N RAIS|   M  |   ,  |   .  |   /  |_RAISE|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   |      | Opt  |Cmmnd | / Space /       \Enter \  | Cmd  | Alt  |      |
  *                   |      |      | Esc  |/ LOWER /         \LOWER \ | Esc  |      |      |
@@ -28,29 +28,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
   KC_GRAVE, KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLASH, \
   KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,  SCMD_T(KC_B), KC_LBRC,  LALT(KC_SPC),  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,  SCMD_T(KC_B), KC_LBRC,  LALT(KC_SPC), LT(_RAISE, KC_N), KC_M,   KC_COMM, KC_DOT,  KC_SLSH,  _RAISE, \
                              _______,KC_LALT, MT(MOD_LGUI, KC_ESC),LT(_LOWER, KC_SPC),  LT(_LOWER, KC_ENT),  MT(MOD_RGUI, KC_ESC), KC_RALT, _______ \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
+ * |  ~`  |  1   |  2   |  3   |  4   |  5   |                    |  6   |  7   |  8   |  9   |   0  | DEL  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |   `  |   !  |   @  |   #  |   $  |   %  |-------.    ,-------|   ^  |   &  |   *  |   (  |   )  |   -  |
- * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------|    |-------|      |   _  |   +  |   {  |   }  |   |  |
+ * |  Tab |   !  |   @  |   #  |   $  |   %  |-------.    ,-------|   ^  |   &  |   *  |   (  |   )  | Ctrl\|
+ * |------+------+------+------+------+------|   [   |    | OptSpc|------+------+------+------+------+------|
+ * |LShift|      |      |      |   _  |S Cmd/-|------|    |-------|   +  |   =  |      |   [{ |  ]}  |_RAISE|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
- *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   |      | Opt  |Cmmnd | /Space  /       \Enter \  | Cmd  |Alt   |      |
+ *                   |      |      | Esc  |/       /         \      \ | Esc  |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 [_LOWER] = LAYOUT( \
   _______, _______, _______, _______, _______, _______,                   _______, _______, _______,_______, _______, _______,\
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
-  KC_GRV, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD, \
-  _______, _______, _______, _______, _______, _______, _______, _______, XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
-                             _______, _______, _______, _______, _______,  _______, _______, _______\
+  KC_GRAVE,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,   KC_8,   KC_9,  KC_0,  KC_BSPC, \
+  KC_TAB, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, LCTL(KC_BSLS), \
+  KC_LSFT, _______, _______, _______,KC_UNDS,SCMD_T(KC_MINS), _______, _______, KC_PLUS, KC_EQL, _______, KC_LBRC, KC_RBRC, _______, \
+                             _______, KC_LALT, MT(MOD_LGUI, KC_ESC), KC_SPC, KC_ENT,  MT(MOD_RGUI, KC_ESC), KC_RALT, _______\
 ),
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
