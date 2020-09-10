d57ce6555c9e726d1a02c9604468ed92db89d2eb
CarlosGz <carlosgz@me.com>
2020-09-10 23:43:25 +0200

    fix alt

--- keyboards/lily58l/keymaps/carguezu/keymap.c
+++ keyboards/lily58l/keymaps/carguezu/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLASH, \
   SCMD_T(KC_Q),KC_A, KC_S,  KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  LALT(KC_SPC),    KC_N, KC_M,   KC_COMM, KC_DOT, KC_SLSH, MO(_RAISE), \
-                    _______,MT(MOD_LGUI, KC_ESC), MO(_RAISE),LT(_LOWER, KC_SPC), LT(_LOWER, KC_ENT), MO(_RAISE), MO(KC_RALT), _______ \
+                    _______,MT(MOD_LGUI, KC_ESC), MO(_RAISE),LT(_LOWER, KC_SPC), LT(_LOWER, KC_ENT), MO(_RAISE), KC_RALT, _______ \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
