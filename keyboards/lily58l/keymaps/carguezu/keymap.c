60dcfe83b42e31bc1b05e41bedade48d41240977
CarlosGz <carlosgz@me.com>
2020-10-05 01:01:49 +0200

    update keymap

--- keyboards/lily58l/keymaps/carguezu/keymap.c
+++ keyboards/lily58l/keymaps/carguezu/keymap.c
@@ -15,11 +15,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  |\  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |SCmd/Q|   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
+ * |SCmd/A|   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------|   [   |    |OptSpc |------+------+------+------+------+------|
- * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  | RAISE|
+ * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |ENTER |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |      | Cmd  |RAISE | / Space /       \Enter \  |RAISE | Opt  |      |
+ *                   |      | Cmd  |RAISE | / Space /       \ DEL  \  |RAISE | Opt  |      |
  *                   |      | Esc  |      |/ LOWER /         \LOWER \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -27,17 +27,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_QWERTY] = LAYOUT( \
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLASH, \
-  SCMD_T(KC_Q),KC_A, KC_S,  KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  LALT(KC_SPC),    KC_N, KC_M,   KC_COMM, KC_DOT, KC_SLSH, MO(_RAISE), \
-                    _______,MT(MOD_LGUI, KC_ESC), MO(_RAISE),LT(_LOWER, KC_SPC), LT(_LOWER, KC_ENT), MO(_RAISE), KC_RALT, _______ \
+  SCMD_T(KC_A),KC_A, KC_S,  KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  LALT(KC_SPC),    KC_N, KC_M,   KC_COMM, KC_DOT, KC_SLSH, KC_ENT, \
+                    _______,MT(MOD_LGUI, KC_ESC), MO(_RAISE),LT(_LOWER, KC_SPC), LT(_LOWER, KC_BSPC), MO(_RAISE), KC_RALT, _______ \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |  ~`  |   !  |  @   |  #   |  $   |   %  |                    |  ^   |  &   |  *   |  (   |   )  | DEL  |
+ * |  ~`  |   1  |  2   |  3   |  4   |   5  |                    |  6   |  7   |  8   |  9   |   0  | DEL  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |SCmd/!|  1   |   2  |   3  |   4  |  5   |-------.    ,-------|   6  |   7  |   8  |   9  |   0  | Ctrl\|
+ * |SCmd/A|   !  |  @   |   #  |   $  |   %  |-------.    ,-------|   ^  |   &  |   *  |  (   |   )  | Ctrl\|
  * |------+------+------+------+------+------|   [   |    |OptSpc |------+------+------+------+------+------|
  * |LShift|Ctrl I|      |      |   _  |   -  |-------|    |-------|   +  |   =  |      |   [{ |  ]}  | RAISE|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
@@ -47,8 +47,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT( \
   _______, _______, _______, _______, _______, _______,                   _______, _______, _______,_______, _______, _______,\
-  KC_GRAVE, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,              KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
-  SCMD_T(KC_EXLM),  KC_1,    KC_2,   KC_3,     KC_4,    KC_5,                        KC_6,   KC_7,   KC_8,    KC_9,    KC_0, LCTL(KC_BSLS), \
+  KC_GRAVE, KC_1,    KC_2,   KC_3,    KC_4,    KC_5,              KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC, \
+  SCMD_T(KC_A), KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,    KC_CIRC,   KC_AMPR,   KC_ASTR, KC_LPRN,  KC_RPRN, LCTL(KC_BSLS), \
   KC_LSFT,LCTL(KC_I),_______, _______,KC_UNDS, KC_MINS, _______, _______, KC_PLUS, KC_EQL, _______, KC_LBRC, KC_RBRC, _______, \
                              _______, _______, _______, _______, _______,  _______, _______, _______ \
 ),
