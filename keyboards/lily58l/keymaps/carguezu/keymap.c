8d318b545e4b989e124b373a5104fa2a9f0feb08
CarlosGz <carlosgz@me.com>
2020-10-09 01:01:19 +0200

    add e and raise

--- keyboards/lily58l/keymaps/carguezu/keymap.c
+++ keyboards/lily58l/keymaps/carguezu/keymap.c
@@ -19,8 +19,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|   [   |    |OptSpc |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |ENTER |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |      | Cmd  |RAISE | / Space /       \ DEL  \  |RAISE | Opt  |      |
- *                   |      | Esc  |      |/ LOWER /         \LOWER \ |      |      |      |
+ *                   |      | Cmd  |RAISE | / Space /       \ DEL  \  |  E   | Opt  |      |
+ *                   |      | Esc  |      |/ LOWER /         \LOWER \ |RAISE |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLASH, \
   SCMD_T(KC_A),KC_A, KC_S,  KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  LALT(KC_SPC),    KC_N, KC_M,   KC_COMM, KC_DOT, KC_SLSH, KC_ENT, \
-                    _______,MT(MOD_LGUI, KC_ESC), MO(_RAISE),LT(_LOWER, KC_SPC), LT(_LOWER, KC_BSPC), MO(_RAISE), KC_RALT, _______ \
+                    _______,MT(MOD_LGUI, KC_ESC), MO(_RAISE),LT(_LOWER, KC_SPC), LT(_LOWER, KC_BSPC), LT(_RAISE, KC_E), KC_RALT, _______ \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
